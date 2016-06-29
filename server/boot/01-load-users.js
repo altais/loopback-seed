b1770b18d20057b4983d3d2f10c73e8b972c4578
Altais <cervantescedric@gmail.com>
2016-06-30 00:10:52 +0200

    Push first version of template loopback-ready

---
+++ server/boot/01-load-users.js
@@ -0,0 +1,70 @@
+'use strict';
+
+// to enable these logs set `DEBUG=boot:01-load-users` or `DEBUG=boot:*`
+var log = require('debug')('boot:01-load-users');
+
+module.exports = function(app) {
+
+  if (app.dataSources.db.name !== 'Memory' && !process.env.INITDB) {
+    return;
+  }
+
+  createDefaultUsers();
+
+  function createDefaultUsers() {
+
+    log('Creating roles and users');
+
+    var User = app.models.UserModel;
+    var Role = app.models.Role;
+    var RoleMapping = app.models.RoleMapping;
+
+    var users = [];
+    var roles = [{
+      name: 'admin',
+      users: [{
+        firstName: 'Admin',
+        lastName: 'User',
+        email: 'admin@admin.com',
+        username: 'admin',
+        password: 'admin'
+      }]
+    }, {
+      name: 'users',
+      users: [{
+        firstName: 'Guest',
+        lastName: 'User',
+        email: 'user@user.com',
+        username: 'user',
+        password: 'user'
+      }]
+    }];
+
+    roles.forEach(function(role) {
+      Role.findOrCreate(
+        {where: {name: role.name}}, // find
+        {name: role.name}, // create
+        function(err, createdRole, created) {
+          (created) ? log('created role', createdRole.name)
+                    : log('found role', createdRole.name);
+          role.users.forEach(function(roleUser) {
+            User.findOrCreate(
+              {where: {username: roleUser.username}}, // find
+              roleUser, // create
+              function(err, createdUser, created) {
+                (created) ? log('created user', createdUser.username)
+                          : log('found user', createdUser.username);
+                createdRole.principals.create({
+                  principalType: RoleMapping.USER,
+                  principalId: createdUser.id
+                }, function(err, rolePrincipal) {
+                  users.push(createdUser);
+                });
+              });
+          });
+        });
+    });
+    return users;
+  }
+
+};
---
+++ server/boot/authentication.js
@@ -0,0 +1,9 @@
+var loopback = require('loopback');
+module.exports = function enableAuthentication(server) {
+  // enable authentication
+  server.enableAuth();
+  server.middleware('auth', loopback.token({
+	  model: server.models.accessToken,
+	  currentUserLiteral: 'me'
+	}));
+};
